5d25bd676bde865df7f17e24489432b775e919e2
anthony <anthonyflores5844@gmail.com>
2024-11-07 14:47:18 -0800

    Reset (pls work)

---
+++ Group-Project/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 0; i == i; i ++) {
+            System.out.printf("Hello Friends #%d%n", i);
+        }
+
+        // Test commit and push
+    }
+}
