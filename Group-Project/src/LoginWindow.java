97316da9959a3d56a2db8cc9214d6fe0038561cf
anthony <anthonyflores5844@gmail.com>
2024-11-07 17:44:13 -0800

    Login window add username and password prompt. NOT WORKING

--- Group-Project/src/LoginWindow.java
+++ Group-Project/src/LoginWindow.java
@@ -1,5 +1,30 @@
 import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 public class LoginWindow extends Frame {
+    TextField username;
+    TextField password;
 
+    public LoginWindow() {
+        Label usernameLabel = new Label("Username:");
+
+        username = new TextField(10);
+        add(username);
+
+//        password = new TextField("Password:");
+//        add(password);
+
+        setLayout(new GridLayout(3, 1));
+        setTitle("Login");
+        setSize(300, 500);
+        setVisible(true);
+
+        addWindowListener(new WindowAdapter() {
+            // Window closing method
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        });
+    }
 }
--- Group-Project/src/Main.java
+++ Group-Project/src/Main.java
@@ -1,9 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        for (int i = 0; i == i; i ++) {
-            System.out.printf("Hello Friends #%d%n", i);
-        }
-
-        // Working once again!
+        new LoginWindow();
     }
 }
