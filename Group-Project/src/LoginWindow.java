0f2bc87e3ef87fa7cb8140ea0c90cc93ebefea75
anthony <anthonyflores5844@gmail.com>
2024-11-27 17:37:41 -0800

    Updated first and last name labels to be within their own panels, which are held within the border style frame

--- Group-Project/src/LoginWindow.java
+++ Group-Project/src/LoginWindow.java
@@ -3,24 +3,45 @@ import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
 public class LoginWindow extends Frame {
-    TextField username;
-    TextField password;
+
+    TextField firstName;
+    TextField lastName;
 
     public LoginWindow() {
-        Label usernameLabel = new Label("Username:");
-        add(usernameLabel);
+        // The border layout for the whole frame
+        setLayout(new BorderLayout());
 
-        username = new TextField(10);
-        add(username);
+        // The first and last name labels will be within their own panel so that they stay next to each other when resizing
+        // The FlowLayout.Center aligns the components of the frame to the center
+        Panel firstNamePanel = new Panel(new FlowLayout(FlowLayout.CENTER));
 
-        Label passwordLabel = new Label("Password:");
-        add(passwordLabel);
+        // Adds the name label on the left of the first name panel
+        Label firstNameLabel = new Label("First name:");
+        firstNamePanel.add(firstNameLabel);
 
-        password = new TextField(10);
-        add(password);
+        // Adds the name text field on the right
+        firstName = new TextField(10);
+        firstNamePanel.add(firstName);
 
-//        setLayout(new GridLayout(4, 1, 5, 5));
-        setLayout(new FlowLayout());
+        Panel lastNamePanel = new Panel(new FlowLayout(FlowLayout.CENTER));
+
+        Label lastNameLabel = new Label("Last name:");
+        lastNamePanel.add(lastNameLabel);
+
+        lastName = new TextField(10);
+        lastNamePanel.add(lastName);
+
+        // Now we put the panels for the name within the panel
+        // The panel within the frame that is contained in a border layout but is grid layout itself.
+        Panel namesPanel = new Panel();
+        namesPanel.setLayout(new GridLayout(2, 1));
+
+        namesPanel.add(firstNamePanel);
+        namesPanel.add(lastNamePanel);
+
+        // Put the composite(?) panel with both labels on the North of the frame
+        add(namesPanel, BorderLayout.NORTH);
+
         setTitle("Login");
         setSize(300, 500);
         setVisible(true);
