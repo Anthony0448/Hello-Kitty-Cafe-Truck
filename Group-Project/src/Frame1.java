32e19fb73c8623001b1337e850c5fe912557afbc
anthony <anthonyflores5844@gmail.com>
2024-11-27 21:47:51 -0800

    Frame 1 skeleton implemented

--- Group-Project/src/Frame1.java
+++ Group-Project/src/Frame1.java
@@ -3,7 +3,6 @@ import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
 public class Frame1 extends Frame {
-
     TextField firstName;
     TextField lastName;
 
@@ -95,10 +94,63 @@ public class Frame1 extends Frame {
         * A "remove" button that removes the item by using the specific line number of that product shown in the invoice
          */
 
+        Panel productGroup = new Panel(new FlowLayout());
+
+        Label productLabel = new Label("Product:");
+        productGroup.add(productLabel);
+
+        TextField productTextField = new TextField(10);
+        productGroup.add(productTextField);
+
+        // To contain the flow panels for product and code
+        Panel composite_Product_Code = new Panel(new GridLayout(2,1));
+
+        // The composite panel of the label and text field for product is added to the top of the bottom panel
+        composite_Product_Code.add(productGroup);
+
+        // Now for the code label and the text input
+        Panel codeGroup = new Panel(new FlowLayout());
+
+        Label codeLabel = new Label("Code:");
+        codeGroup.add(codeLabel);
+
+        TextField codeTextField = new TextField(10);
+        codeGroup.add(codeTextField);
+
+        composite_Product_Code.add(codeGroup);
+
+        Panel bottomPannel = new Panel(new GridLayout(4,1));
+
+        // Quantity middle panel for the bottom panel
+        Panel quantityGroup = new Panel(new FlowLayout());
+
+        Label quantityLabel = new Label("Quantity:");
+        quantityGroup.add(quantityLabel);
+
+        TextField quantityTextField = new TextField(10);
+        quantityGroup.add(quantityTextField);
+
+        // Add and remove buttons for the bottom part of the bottom panel
+        Panel addRemoveButtons = new Panel(new FlowLayout());
+
+        Button addButton = new Button("Add");
+        addRemoveButtons.add(addButton);
+
+        Button removeButton = new Button("Remove");
+        addRemoveButtons.add(removeButton);
+
+        // Spacer
+        bottomPannel.add(new Label());
+
+        bottomPannel.add(composite_Product_Code);
+        bottomPannel.add(quantityGroup);
+        bottomPannel.add(addRemoveButtons);
+
+        add(bottomPannel);
 
 
         setTitle("Login");
-        setSize(300, 500);
+        setSize(300, 700);
         setVisible(true);
 
         addWindowListener(new WindowAdapter() {
